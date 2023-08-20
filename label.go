170658e387c240d9ba5a3554bad32b9fc9d043d5
Samy A <matwachich@gmail.com>
2023-08-20 16:04:42 +0100

    Label

---
+++ label.go
@@ -0,0 +1,29 @@
+package wx
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Label struct {
+	widget.Label
+
+	minWidth float32
+}
+
+func (w *Label) SetMinWidth(width float32) {
+	w.minWidth = width
+}
+
+func (w *Label) SetMinWidthText(text string) {
+	w.minWidth = fyne.MeasureText(text, theme.TextSize(), w.Label.TextStyle).Width
+}
+
+func (w *Label) MinSize() fyne.Size {
+	sz := w.Label.MinSize()
+	if sz.Width < w.minWidth {
+		sz.Width = w.minWidth
+	}
+	return sz
+}
